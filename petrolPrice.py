17c711e919171c4c204c15b15f4813f3fd599769
ashdvlps <ashdvlps@gmail.com>
2018-05-05 11:35:16 -0700

    push new file

--- petrolPrice.py
+++ petrolPrice.py
@@ -17,30 +17,25 @@ kolkata_prices = []
 mumbai_prices = []
 chennai_prices = []
 petrol_prices = []
-months_mapping = {"January": "01", "February": "02", "March": "03", 
+months_mapping = {"January": "01", "February": "02", "March": "03",
                   "April": "04", "May": "05", "June": "06", "July": "07",
                   "August": "08", "September": "09", "October": "10",
                   "November": "11", "December": "12"}
 for row in rows:
     cells = row.find_all('td')
     date = months_mapping[cells[0].get_text().replace('\n', '').split(',')[0]
-                                  .split(' ')[0]]+'-'+cells[0].get_text()
-                                  .replace('\n', '').split(',')[0]
-                                  .split(' ')[1]+'-'+cells[0].get_text()
-                                  .replace('\n', '').split(',')[1]
-                                  .replace(' ', '')
-    # print(date)
+                                  .split(' ')[0]]+'-'+cells[0].get_text().replace('\n', '').split(',')[0].split(' ')[1]+'-'+cells[0].get_text().replace('\n', '').split(',')[1].replace(' ', '')
+
     delhi_price = cells[1].get_text().replace('\n', '')
     kolkata_price = cells[2].get_text().replace('\n', '')
     mumbai_price = cells[3].get_text().replace('\n', '')
     chennai_price = cells[4].get_text().replace('\n', '')
-    # print(date)
-    petrol_price = [date, delhi_price,kolkata_price, mumbai_price,
+
+    petrol_price = [date, delhi_price, kolkata_price, mumbai_price,
                     chennai_price]
     petrol_prices.append(petrol_price)
 
-# print(petrol_prices)
-# petrol_prices = [dates, delhi_prices, kolkata_prices, mumbai_prices, chennai_prices]
+
 petrol_prices.pop(0)
 df = pd.DataFrame(petrol_prices)
 df.columns = ['dates', 'Delhi', 'Kolkata', 'Mumbai', 'Chennai']
